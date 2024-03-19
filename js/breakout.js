1481e1c138ef2ba402a6e5ffaa129ddc06d78712
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-03-19 17:18:21 +0000

    Tue, Mar 19, 2024, 10:18 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -6,7 +6,27 @@ closebtn = document.getByElementById('close-btn')
 canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
+//create ball properties
+ball={
+    x: canvas.width / 2,
+    y:canvas.heigght / 2,
+    size:10,
+    speed:4;
+    dx:4,
+    dy:-4,
+}
+//draw ball on canvas
+function drawball(){
+    
+}
 
+
+
+
+
+
+
+//Buttons
 rulesbtn.addEventListener('click',() =>{
     rules.addClassList('show')
 })
