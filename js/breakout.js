1d68a3dd69215e374287a50e2c990020c013a172
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-03-19 17:01:20 +0000

    Tue, Mar 19, 2024, 10:01 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,5 +1,6 @@
 rules= document.getByElementById('.rules')
 rules-btn = document.getByElementById('.rules-btn')
+close-btn = document.getByElementById('.')
 
 rules-btn.addEventListener(click, =>{
     rules.addClassList('show')
