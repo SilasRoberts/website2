3bef1fbb1af9cba940a88bf10f80eb138b306bde
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-03-19 17:14:48 +0000

    Tue, Mar 19, 2024, 10:14 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -2,9 +2,17 @@ rules= document.getByElementById('rules')
 rulesbtn = document.getByElementById('rules-btn')
 closebtn = document.getByElementById('close-btn')
 
+
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+
 rulesbtn.addEventListener('click',() =>{
     rules.addClassList('show')
 })
 closebtn.addEventListener('click',() =>{
     rules.removeClassList('show')
 })
+
+
+
