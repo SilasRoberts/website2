a473b11133f6c6e1448aeb76de531f2a2bb43336
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-04-10 09:07:25 -0700

    insect C

--- js/insect.js
+++ js/insect.js
@@ -1,3 +1,23 @@
-const screens = document.QuerySelectorAll('.screen')
+const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
 
-screen[0].classList.add('up')
+start_btn.addEventListener('click',() =>{
+screens[0].classList.add('up')
+})
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () =>{
+    const img = btn.querySelector('img')
+    const alt = img.getAttribute('alt')
+    const src = img.getAttribute('src')
+    screens[1].classList.add('up')}
+    )
+})
+
