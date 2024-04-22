9582fd7356c3787a8d9527c5e6d54d893abd6772
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-04-22 16:56:39 +0000

    Mon, Apr 22, 2024, 9:56 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -11,4 +11,21 @@ const word = ['application', 'programming', 'interface', 'wizard']
 let selectedIndex =  Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
 
-console.log(selectedWord)
+const correctLetters = []
+const wrongLetters = []
+
+//show hiddenword
+function displayWord(){
+    wordEL.innerHTML = `
+    ${selectedWord
+        .split('')
+        .map(letter => `
+        <span class="letter">
+          ${correctLetters.includes(letter) ? letter : ''}
+        <span>
+        `)}
+
+    `
+}
+
+displayWord()
