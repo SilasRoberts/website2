0dbbd852074215f28e06c71043d644f5a00864a1
SilasRoberts <142546763+SilasRoberts@users.noreply.github.com>
2024-05-17 18:31:30 -0700

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+button = document.getElementById('start-button')
+stuff = document.getElementById('stuff')
+
+
+button.addEventListener('click', () => {
+button.classList.add('invisible')
+stuff.classList.add('visible')
+})
